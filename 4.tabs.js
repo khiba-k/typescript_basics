fd9b057dea22c24e6ef8ef34c0163aac22660ecf
khiba-k <khibalpemame@gmail.com>
2024-04-27 14:24:40 +0200

    created tabs

---
+++ 4.tabs.js
@@ -0,0 +1,33 @@
+function printOne() {
+    var hidebodyTwo = document.getElementById("bodyTwo");
+    if (hidebodyTwo)
+        hidebodyTwo.style.display = "none";
+    var hidebodyThree = document.getElementById("bodyThree");
+    if (hidebodyThree)
+        hidebodyThree.style.display = "none";
+    var getbodyOne = document.getElementById("bodyOne");
+    if (getbodyOne)
+        getbodyOne.style.display = "block";
+}
+function printTwo() {
+    var hidebodyOne = document.getElementById("bodyOne");
+    if (hidebodyOne)
+        hidebodyOne.style.display = "none";
+    var hidebodyThree = document.getElementById("bodyThree");
+    if (hidebodyThree)
+        hidebodyThree.style.display = "none";
+    var getbodyTwo = document.getElementById("bodyTwo");
+    if (getbodyTwo)
+        getbodyTwo.style.display = "block";
+}
+function printThree() {
+    var hidebodyOne = document.getElementById("bodyOne");
+    if (hidebodyOne)
+        hidebodyOne.style.display = "none";
+    var hidebodyTwo = document.getElementById("bodyTwo");
+    if (hidebodyTwo)
+        hidebodyTwo.style.display = "none";
+    var getbodyThree = document.getElementById("bodyThree");
+    if (getbodyThree)
+        getbodyThree.style.display = "block";
+}
---
+++ 4.tabs.tsx
@@ -0,0 +1,40 @@
+function printOne() {
+    var hidebodyTwo = document.getElementById("bodyTwo");
+    if (hidebodyTwo)
+        hidebodyTwo.style.display = "none";
+    var hidebodyThree = document.getElementById("bodyThree");
+    if (hidebodyThree)
+        hidebodyThree.style.display = "none";
+
+    var getbodyOne = document.getElementById("bodyOne");
+    if (getbodyOne)
+        getbodyOne.style.display = "block";
+}
+
+function printTwo() {
+    var hidebodyOne: HTMLElement | null = document.getElementById("bodyOne");
+    if (hidebodyOne)
+        hidebodyOne.style.display = "none";
+
+    var hidebodyThree: HTMLElement | null = document.getElementById("bodyThree");
+    if (hidebodyThree)
+        hidebodyThree.style.display = "none";
+
+    var getbodyTwo = document.getElementById("bodyTwo");
+    if (getbodyTwo)
+        getbodyTwo.style.display = "block";
+}
+
+function printThree() {
+    var hidebodyOne = document.getElementById("bodyOne");
+    if (hidebodyOne)
+        hidebodyOne.style.display = "none";
+
+    var hidebodyTwo = document.getElementById("bodyTwo");
+    if (hidebodyTwo)
+        hidebodyTwo.style.display = "none";
+
+    var getbodyThree = document.getElementById("bodyThree");
+    if (getbodyThree)
+        getbodyThree.style.display = "block";
+}
